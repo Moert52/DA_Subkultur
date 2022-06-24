414b6d8097ab57f5721153314c2d06395c0f10f7
Moert52 <mcetinkaya@tsn.at>
2022-06-25 00:21:24 +0200

    Subkultur

---
+++ da_subkultur/img.py
@@ -0,0 +1,199 @@
+from pysolr import Solr
+import os
+import logging
+import hashlib
+import datetime
+import xml.etree.ElementTree as ET
+import elementpath
+from PIL import Image, ImageDraw, ImageFont
+import glob
+from lxml import etree
+import io
+import json
+import glob
+import re
+
+DOCUMENT_SITE_ID = 'Cultblech'
+DOCUMENT_URL = r'C:\Users\mertc\Desktop\HTL - Fächer\Diplomarbeit\Test-tesseract\Cultblech_1'
+
+class Processor(object):
+
+    def __init__(self, solr_server_url):
+        self.server = Solr(solr_server_url)
+
+    def process(self, fname, title):
+        base, _ = os.path.splitext(os.path.basename(fname))
+        url = DOCUMENT_URL + r"\%s" % (base) + '.pdf'
+        fPath = os.path.join(DOCUMENT_URL,fname)
+        fp = open(fPath, encoding="iso-8859-1")
+        content = ''
+        for line in fp:
+            s = line.strip()
+            print(s)
+            if s and not s.startswith(('**', '==', '--')):
+                content += s #str(s.encode(encoding='utf-8', errors='strict'))
+        fp.close()
+        document_id = u"%s-%s" % (DOCUMENT_SITE_ID, title)
+        logging.info("new document: %s" % (document_id,))
+        t = os.path.getmtime(fPath)
+        doc = {
+            'id': hashlib.sha1(document_id.encode('utf-8')).hexdigest(),
+            'site': DOCUMENT_SITE_ID,
+            'url': url,
+            'title': title,
+            'content': content,
+            'last_modified': str(datetime.datetime.fromtimestamp(t))
+        }
+
+        docStr = json.dumps(doc)
+        print(docStr)
+        try:
+            self.server.add([doc])
+            self.server.commit()
+        except (IOError, Exception) as e:
+            if not self.silently_fail:
+                raise
+
+            self.log.error("Failed to add documents to Solr: %s", e)
+
+    def delete(self, title):
+        document_id = u"%s-%s" % (DOCUMENT_SITE_ID, title)
+        logging.info("new document: %s" % (document_id,))
+        print(hashlib.sha1(document_id.encode('utf-8')).hexdigest())
+        self.server.delete(id=str(hashlib.sha1(document_id.encode('utf-8')).hexdigest()))
+        self.server.commit()
+
+    def search(self, title):
+        results = self.server.search('content:*%s* title:*%s* site:*%s*' % (title, title, title), sort='order_i asc', rows=500,)
+        self.server.commit()
+        z = 0
+        arr = []
+        title.split()
+        for t in title:
+            t.capitalize()
+
+        title = title + ' %s ' % title.upper() + '%s ' % title.lower()
+        print(title)
+        try:
+            for result in results:
+                z += 1
+                #print('URL: %s' % result['url'])
+                arr.append(result['url'])
+            highlight(title, arr)
+        except Exception as e:
+            self.log.error("Kein Content vorhanden bei %s \nError: %s", result['title'], e)
+
+        print('Es wurden %s Eintraege gefunden' % z)
+
+
+    def delAll(self):
+        self.server.delete(q='*:*')
+        print("Alles gelöscht")
+
+
+def addAll(dir, p, title):
+    z = 0
+    for f in glob.glob("%s/*.txt" % dir):
+        ff = os.path.join(dir, f)
+        file = ff.removeprefix(dir+'\\')
+        print(file)
+
+        p.process(file, title+'_%s' % str(z))
+        z=z+1
+
+def extract_pdf(fname):
+    pdffile = fname
+    doc = fitz.open(pdffile)#
+
+    for i in range(doc.page_count):
+        page = doc.load_page(i)  # number of page
+        mat = fitz.Matrix(5,5) # To get higher resolution
+        pix = page.get_pixmap(matrix=mat)
+        dir, extension = os.path.splitext(pdffile)
+        Path(dir).mkdir(exist_ok=True)
+        pix.save("%s/%d.png" % (dir,i))
+
+def highlight(string, dir):
+    #print(os.path.basename(dir[0]))
+    #dic = dir[0].removesuffix('\\'+ os.path.basename(dir[0]))
+    #dic = dic.replace("\\\\", "\\")
+    #print(dic)
+    #for f in glob.glob("%s/*.png" % dic):
+        #ff = os.path.join(dic, f)
+        #highlight_image(ff, '%s_alto_neu.xml' % ff, string)
+    clearFolder()
+    for f in dir:
+        ff = f[0].removesuffix('_text.pdf')
+        ff = ff.replace("\\\\", "\\")
+        highlight_image(ff, '%s_alto_neu.xml' % ff, string)
+
+def highlight_image(img, xml, string):
+    #print(xml)
+    stri = string.split()
+    #print(stri)
+    root = ET.parse(xml)
+    image = Image.open(img)
+
+    anz = 1
+    for p in elementpath.select(root, '//String', {'' : 'http://www.loc.gov/standards/alto/ns-v3#'}):
+        st = str(p.attrib["CONTENT"])
+        for e in stri:
+            if re.search(e, st):
+                gid = p.attrib["ID"]
+                x0 = int(p.attrib["HPOS"])
+                y0 = int(p.attrib["VPOS"])
+                x1 = int(x0 + int(p.attrib["WIDTH"]))
+                y1 = int(y0 + int(p.attrib["HEIGHT"]))
+                shape = [x0, y0,x1,y1 ]
+
+
+                TINT_COLOR = (0, 0, 0)  # Black
+                TRANSPARENCY = .50  # Degree of transparency, 0-100%
+                OPACITY = int(255 * TRANSPARENCY)
+
+
+                anz+=1
+                #print (gid, shape)
+                image = image.convert("RGBA")
+                overlay = Image.new('RGBA', image.size, TINT_COLOR + (0,))
+                draw = ImageDraw.Draw(overlay)  # Create a context for drawing things on it.
+                #draw.text((x1, y1), gid, align="right", font=ImageFont.load_default(), fill="green")
+                draw.rectangle(shape, fill=(255, 127, 127, 127), outline="black")
+
+                # Alpha composite these two images together to obtain the desired result.
+                image = Image.alpha_composite(image, overlay)
+                image = image.convert("RGB")  # Remove alpha for saving in jpg format.
+                dic = img.removeprefix(r'C:\Users\mertc\Desktop\HTL - Fächer\Diplomarbeit\Test-tesseract')
+                #print('JA:' +dic)
+                file = dic + '_suche.jpg'
+                file = file.replace("\\", "-")
+                #print(file)
+                image.save(r'C:\Users\mertc\Desktop\HTL - Fächer\Diplomarbeit\Test-tesseract\suche\\' + file)
+                image.save(img+"_suche.jpg")
+                print(img+"_suche.jpg")
+
+
+def clearFolder():
+    folder = r'C:\Users\mertc\Desktop\HTL - Fächer\Diplomarbeit\Test-tesseract\suche'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+if __name__ == '__main__':
+
+    p = Processor('http://localhost:8983/solr/test')
+
+    #p.delAll()
+    #addAll(DOCUMENT_URL, p, )
+    #p.process('0.png_text.txt', 'Cultblech_Logo_0')
+    #p.delete('Cutblech_Logo_0')
+
+    p.search('Innsbruck')
+    p.server.commit()
---
+++ da_subkultur/main.py
@@ -0,0 +1,156 @@
+import fitz #https://pymupdf.readthedocs.io/en/latest/tutorial.html
+import os
+from pathlib import Path
+import pytesseract
+import xml.etree.ElementTree as ET
+import elementpath
+from PIL import Image, ImageDraw, ImageFont
+import glob
+from lxml import etree
+import io
+import aspose.words as aw
+
+
+#Hier wird Tesseract definiert
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+#hier werden alle xml Files der Text ausgelesen und die Bilder mit einem Rahmen gekennzeichnet
+def highlight_images(dir):
+    for f in glob.glob("%s/*.png" % dir):
+        ff = os.path.join(dir, f)
+        print(ff)
+
+
+        highlight_image(ff, '%s_alto_neu.xml' % ff)
+        get_Text(ff, '%s_alto_neu.xml' % ff)
+        #output = aw.Document()
+        #input = aw.Document(ff + "_text.txt")
+        # Append the source document to the end of the destination document.
+        #output.append_document(input, aw.ImportFormatMode.KEEP_SOURCE_FORMATTING)
+        #output.save(ff+"Output.docx")
+
+
+
+#hier erfolgt die Gekennzeichnung der Bilder mittels einem Rahmen
+def highlight_image(img, xml):
+    print(xml)
+    root = ET.parse(xml)
+    image = Image.open(img)
+    draw = ImageDraw.Draw(image)
+    anz = 1
+    for p in elementpath.select(root, '//Illustration', {'' : 'http://www.loc.gov/standards/alto/ns-v3#'}):
+        gid = p.attrib["ID"]
+        x0 = int(p.attrib["HPOS"])
+        y0 = int(p.attrib["VPOS"])
+        x1 = int(x0 + int(p.attrib["WIDTH"]))
+        y1 = int(y0 + int(p.attrib["HEIGHT"]))
+        shape = [x0, y0,x1,y1 ]
+
+        crop_img(img, shape, anz)
+        anz+=1
+        print (gid, shape)
+        draw.text((x1, y1), gid, align="right", font=ImageFont.load_default(), fill="green")
+        draw.rectangle(shape, fill=None, outline ="green")
+    image.save(img + "_col.jpg")
+
+#Hier wird der Text vom xml File ausgelesen und in einem Text dokument abgespeichert
+#(die Zeilenumbrüche werden berücksichtigt)
+def get_Text(img, xml):
+    print(xml)
+    root = ET.parse(xml)
+    anz = 0
+    txt = []
+    arr = []
+    done = False
+    althoe=0
+    #for l in elementpath.select(root, '//TextLine', {'': 'http://www.loc.gov/standards/alto/ns-v3#'}):
+        #an = 0
+        #print(str(l.attrib["CONTENT"]))
+    for t in elementpath.select(root, '//String', {'': 'http://www.loc.gov/standards/alto/ns-v3#'}):
+        st = str(t.attrib["CONTENT"])
+        print(st)
+        height = int(t.attrib["VPOS"])
+        if done == False:
+            arr.append(st)
+            done = True
+            althoe = height
+        elif (height > (althoe-20)) &(height< (althoe+20))&(done ==True):
+            arr.append(st)
+            althoe = height
+        else:
+            anz = anz + 1
+            txt.append(arr)
+            arr = []
+            arr.append(st)
+            althoe = height
+    txt.append(arr)
+    print(txt)
+    te=''
+    for i in txt:
+        for j in i:
+            te = te + str(j) + " "
+        te = te +" \n"
+    print(te)
+
+    open(img + "_text.txt", "w",encoding="iso-8859-1").write(te)
+
+#Hier werden die Bilder von den einzelnen Dokumenten ausgeschneidet und dann abgespeichert
+def crop_img(img, shape, anz):
+    im = Image.open(img)
+    im1 = im.crop((shape))
+    #im1.show()
+    if im1.width == 0 or im1.height == 0:
+        print("Fehler beim Bildausschnitt erstellen")
+    else:
+        im1.save(img + "_pic_"+str(anz)+".jpg")
+
+#Hier werden alle Seiten des pdfs File in einzelne png - Bilder abgespeichert
+def extract_pdf(fname):
+    pdffile = fname
+    doc = fitz.open(pdffile)
+    for i in range(doc.page_count):
+        page = doc.load_page(i)  # number of page
+        mat = fitz.Matrix(5,5) # To get higher resolution
+        pix = page.get_pixmap(matrix=mat)
+        dir, extension = os.path.splitext(pdffile)
+        Path(dir).mkdir(exist_ok=True)
+        pix.save("%s/%d.png" % (dir,i))
+
+#Hier erfolgt die ocr der seite, aschnließend werden die Informationen in einem xml - File abgespeichert
+def do_ocr(f):
+    print(f)
+    res = pytesseract.image_to_alto_xml(f, lang='deu')
+    print("Ergebnis")
+    print(res)
+
+    #res = replace(res)
+    print(res)
+    fp = open("%s_alto.xml" % f, "wb")
+    fp.write(res)
+    fp.close()
+
+    #fp = f
+    fp_out = '0.png_alto_utf-8.xml'
+    fp = io.open("%s_alto.xml" % f, mode="r", encoding="iso-8859-1")
+    s = fp.read()
+    print(s)
+    open("%s_alto_neu.xml" % f, 'w', encoding="utf-8").write(s)
+
+#Hier wird bei seiten im Ordner die Ocr - Methode durchgeführt
+def do_ocr_dir(dir):
+    for f in glob.glob("%s/*.png" % dir):
+        ff = os.path.join(dir, f)
+        do_ocr(ff)
+
+#Hier werden alle Methoden zusammengefasst, welches dann bei allen pdf Files im Ordner durchgeführt wird
+def process_dir(dir):
+    for f in glob.glob("%s/*.pdf" % dir):
+        ff = os.path.join(dir, f)
+        print (ff)
+        dir, extension = os.path.splitext(ff)
+        extract_pdf(ff)
+        do_ocr_dir(dir)
+        highlight_images(dir)
+
+if __name__ == '__main__':
+    process_dir('C:\\Users\mertc\Desktop\HTL - Fächer\Diplomarbeit\\Test-tesseract')
