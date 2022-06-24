4ed064c0158d73ed5f2f8a24462047cf48b6defc
Moert52 <mcetinkaya@tsn.at>
2022-06-25 01:10:09 +0200

    Subkultur

---
+++ da_subkultur/Subkultur - Webserver.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource, Api
+from flask import Flask, render_template, session, jsonify
+
+app=Flask(__name__,template_folder='templates')  # Die Flask-Anwendung
+api = Api(app)  # Die Flask API
+app.secret_key = '_5#y2L"F4Q8z/n/xec] /'
+
+@app.route('/')
+def Start():
+    return render_template("Start.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
