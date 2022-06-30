cd4314b8e21b629e0ca78207e08917c946b56c48
LeonardoDjurdjevic <ldjurdjevic@tsn.at>
2022-06-30 15:51:28 +0200

    Subkulturarchiv

--- da_subkultur/models/userDB.py
+++ da_subkultur/models/userDB.py
@@ -21,7 +21,7 @@ def insert(user):
         c.execute("INSERT INTO users (id, email, password) VALUES (?, ?, ?)", (user.id, user.email, user.password))
         conn.commit()
     except sqlite3.Error as error:
-        print("An error occurred while connecting to SQLite", error)
+        print("An error occurred while inserting a user", error)
     finally:
         conn.close()
 
@@ -30,11 +30,11 @@ def getUser(id):
     try:
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
-        c.execute("SELECT * FROM users WHERE id={}".format(id))
+        c.execute("SELECT * FROM users WHERE id=?", id)
         user = c.fetchone()
         return user
     except sqlite3.Error as error:
-        print("An error occurred while connecting to sqlite", error)
+        print("An error occurred while getting a user", error)
     finally:
         conn.close()
     return -1
@@ -44,13 +44,34 @@ def deleteUser(id):
     try:
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
-        c.execute("DELETE FROM users WHERE id={}".format(id))
+        c.execute("DELETE FROM users WHERE id=?", id)
         conn.commit()
     except sqlite3.Error as error:
-        print("An error occurred while connecting to SQLite", error)
+        print("An error occurred while deleting a user", error)
     finally:
         conn.close()
 
 
+def login(email, password):
+    try:
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users WHERE email=? and password=?", (email, password))
+        return 1
+    except sqlite3.Error as error:
+        print("An error occurred while login", error)
+    finally:
+        conn.close()
+    return 0
+
+
 if __name__ == "__main__":
-    print(getUser(1))
+    print("Enter EMail: ")
+    email = input()
+    print("Enter Password: ")
+    password = input()
+
+    if login(email, password) == 1:
+        print("logged in")
+    else:
+        print("login failed")
